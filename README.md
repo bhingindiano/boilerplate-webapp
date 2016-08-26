c4c51233ff70e787c34006e71d3216736c72c1cb
Bhing <maria.lourdes.indiano@schibsted.se>
2016-08-26 14:13:30 +0200

    Basic setup

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# boilerplate-webapp
+# Webapp Boilerplate
+Basic setup for a static web application.
+
+## Includes
+* Babel for ES6 transpiling
+* Sass (node-sass)
+* Bootstrap (thru cdn)
+* EJS templating (http://www.embeddedjs.com/)
+
+
+## Get started
+1. Fork this repo
+2. Run `npm install` to install dependencies (webpack & babel-cli needs to be global)
+3. Run `npm run dev`
+4. Access http://localhost/
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var path = require('path');
+
+const config = {
+    entry: './src/js/index.js',
+    output: {
+        path: 'dist',
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015']
+                }
+            },
+            {
+                test: /\.scss$/,
+                loaders: ["style", "css", "sass"]
+            },
+            { test: /\.css$/, loader: 'style-loader!css-loader' },
+            { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
+            { test: /\.(woff|woff2)$/, loader:"url?prefix=font/&limit=5000" },
+            { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
+            { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml" },
+        ]
+    }
+};
+
+module.exports = config
